960a99b03db19fb725082fe59d9a6488f5c662b5
AriRice <97973849+AriRice@users.noreply.github.com>
2022-01-21 14:03:15 -0600

    Update Step_03a_coverage.sh

--- Step_03a_coverage.sh
+++ Step_03a_coverage.sh
@@ -17,7 +17,7 @@ KU30946_Coconino_final.bam KU30948_Pinalenos_final.bam KU30950_Pinalenos_final.b
 KU30952_Coconino_final.bam KU30957_SantaRita_final.bam KU30958_SantaRita_final.bam KU31305_Pinalenos_final.bam \
 KU31308_SantaRita_final.bam KU31310_Prescott_final.bam KU31311_Apache_final.bam KU31313_Chiricahua_final.bam \
 KU31315_Apache_final.bam KU31316_SantaCatalina_final.bam KU31406_SantaCatalina_final.bam KU31410_Chiricahua_final.bam \
-KU31415_Apache_final.bam KU314210_Chiricahua_final.bam KU31426_SantaCatalina_final.bam > sitta_coverage.txt
+KU31415_Apache_final.bam KU31421_Chiricahua_final.bam KU31426_SantaCatalina_final.bam > sitta_coverage.txt
 
 # break up the depth files into single column files for each individual (locations dropped)
 
