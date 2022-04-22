53aab37c9c4bb4ede56396f7e852d86a72ec45d8
AriRice <97973849+AriRice@users.noreply.github.com>
2022-04-22 14:59:53 -0500

    Update Bayescenv_temps.sh

--- Bayescenv_temps.sh
+++ Bayescenv_temps.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #SBATCH --chdir=./
 #SBATCH --job-name=bayescenv_test
-#SBATCH --nodes=1 --ntasks=1
-#SBATCH --partition quanah
+#SBATCH --nodes=1 --ntasks=125
+#SBATCH --partition nocona
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --mail-user=arrice@ttu.edu
